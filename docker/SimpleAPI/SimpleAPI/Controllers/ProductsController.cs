c3ac4359975d64a0ed7ba936950e6f724c35c07e
hoo ws <geiwojina@gmail.com>
2024-10-13 18:06:52 +0800

    add ignore

---
+++ docker/SimpleAPI/SimpleAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleAPI.Models;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly OnlineShopDbContext _context;
+
+        public ProductsController(OnlineShopDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
